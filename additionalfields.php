be6382c4e8fa36e7ae4b123de32a8a8254feae1a
Marian Lorscheid <118979005+Mlorscheid@users.noreply.github.com>
2023-01-19 01:47:58 +0100

    WHMCS file

---
+++ additionalfields.php
@@ -0,0 +1,15 @@
+<?php
+ // .DE
+$additionaldomainfields[".de"][] = array(
+ "Name" => "Tax ID",
+ "Remove" => true,
+);
+?>
+
+<?php
+ // .IT
+$additionaldomainfields[".it"][] = array(
+ "Name" => "Tax ID",
+ "Remove" => true,
+);
+?>
